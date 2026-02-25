4e1c9379e5d4dbbf32d2c11d09345ac7119ab631
Cheta Nzeogu <chetanzeogu410@gmail.com>
2026-02-24 20:47:11 -0800

    hono server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors'
 
-const app = new Hono()
-
+const app = new Hono();
+app.use('/fortune', cors())
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+	return c.text('Hello Hono!');
+});
 
-app.get('/fortune', (c) => {
-  
-})
+app.get('/fortune', async (c) => {
+	try {
+		const response = await fetch(
+			'https://ai.hackclub.com/proxy/v1/responses',
+			{
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${process.env.apiKey}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					model: 'google/gemini-2.5-flash',
+					input: 'Generate a random fortune like the ones that are given at panda express. Keep it under 100 words',
+					max_output_tokens: 400,
+				}),
+			},
+		);
 
-export default app
+		const result = await response.json();
+		return c.text(result.output[0].content[0].text)
+	} catch (error) {
+		return c.json({ error: 'Request timed out', detail: String(error) });
+	}
+});
+
+export default app;
--- src/lib/Generator.svelte
+++ src/lib/Generator.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { OpenRouter } from '@openrouter/sdk';
 	import scroll from '../assets/cookie.png';
+	const content = "lorem ipsum, hi, lorem upsum"
+	const generate = async () => {
 
+	};
 
 </script>
 
@@ -9,27 +12,7 @@
 	<button class="generate-btn"> Generate your fortune</button>
 	<div class="scroll-container">
 		<span class="scroll-content">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-			aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-			culpa qui officia deserunt mollit anim id est laborum. Curabitur
-			pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius,
-			turpis et commodo pharetra, est eros bibendum elit, nec luctus magna
-			felis sollicitudin mauris. Integer in mauris eu nibh euismod
-			gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-			nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-			culpa qui officia deserunt mollit anim id est laborum. Curabitur
-			pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius,
-			turpis et commodo pharetra, est eros bibendum elit, nec luctus magna
-			felis sollicitudin mauris. Integer in mauris eu nibh euismod
-			gravida.
+			{content}
 		</span>
 		<img src={scroll} alt="A cookie" class="scroll" />
 	</div>
